26b306869ecb205c23026c3acac8ff02348e51fb
Rikard Engström <rikardengstrom89@gmail.com>
2025-03-19 15:48:37 +0100

    Trying to add logic if a player guess the correct word.

--- wordleFeedback.test.js
+++ wordleFeedback.test.js
@@ -2,18 +2,23 @@ import { expect, describe, test } from "@jest/globals";
 import wordleFeedback from "./wordleFeedback";
 
 /*
-///
-/Test-strategy for algoritm A (wordleFeedback.js)
-///
+//////////
+///Test-strategy for algoritm A (wordleFeedback.js)
+/////////
 
 1. Length control
 -Verifies that the function return an error message when the guess
 has another length than the "secret-word".
 
+2. Correct matches
+
 */
 
+////
+//TEST 1
+////
+
 describe("wordleFeedback", () => {
-  //Test 1 checks the length for a short guess.
   it("Returns a error message when the guess is to short", () => {
     const result = wordleFeedback("HEJ", "CYKEL");
     expect(result).toBe("Din gissning måste innehålla 5 antal bokstäver.");
@@ -23,4 +28,8 @@ describe("wordleFeedback", () => {
     const result = wordleFeedback("ELEFANTER", "CYKEL");
     expect(result).toBe("Din gissning måste innehålla 5 antal bokstäver.");
   });
+
+  ////
+  //TEST 2
+  ////
 });
