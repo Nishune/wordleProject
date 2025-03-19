3ac9fa09e024ea6b4ac6865311570af270ae04eb
Rikard Engström <rikardengstrom89@gmail.com>
2025-03-19 20:29:25 +0100

    commented some code so i dont forget.

--- wordleFeedback.js
+++ wordleFeedback.js
@@ -1,30 +1,38 @@
 export default function wordleFeedback(guess, correctWord) {
+  //Removes all blank spaces and converts to uppercase.
   guess = guess.replace(/\s/g, "").toUpperCase();
   correctWord = correctWord.replace(/\s/g, "").toUpperCase();
-
+  //if guess is not equal to "secret word"-length, returns message.
   if (guess.length !== correctWord.length) {
     return `Din gissning måste innehålla ${correctWord.length} antal bokstäver.`;
   }
-
+  //if guess equals the "secret word", return message.
   if (guess === correctWord) {
     return "Grattis! Du har gissat rätt ord!";
   }
-
+  //Array that holds the result for each letter.
   const result = [];
-
+  //splits the guess & correctword to an array with separate characters.
   const guessArray = guess.split("");
   const correctWordArray = correctWord.split("");
 
+  //Loops through every character in the guess.
   for (let i = 0; i < guessArray.length; i++) {
+    //stores the "active" character from the guess.
     const currentChar = guessArray[i];
-
+    //if the characters position is in the correct word and if the character matches
+    //then we pushes it into the result with status correct.
     if (i < correctWordArray.length && currentChar === correctWordArray[i]) {
       result.push({
         letter: currentChar,
         result: "correct",
       });
-
+      //marks the position in the correct word as used (null)
+      //this is done to avoid that the same character matches again in the next iteration.
       correctWordArray[i] = null;
+
+      //if the character is not correct on this position, mark it as pending as a temporary
+      //status which will be updated in the second loop.
     } else {
       result.push({
         letter: currentChar,
@@ -34,19 +42,23 @@ export default function wordleFeedback(guess, correctWord) {
   }
 
   for (let i = 0; i < guessArray.length; i++) {
+    //Only loop through character that still are marked as "pending"
     if (result[i].result === "pending") {
       const currentChar = guessArray[i];
-      const correctIndex = correctWordArray.indexOf(currentChar);
+      //Looks through the "secret word" after the actual character, if found, return its position, otherwise -1.
+      const positionInSecretWord = correctWordArray.indexOf(currentChar);
 
-      if (correctIndex !== -1) {
+      if (positionInSecretWord !== -1) {
+        //if character is in the word, but at wrong position, mark as misplaced.
         result[i].result = "misplaced";
-        correctWordArray[correctIndex] = null;
+        correctWordArray[positionInSecretWord] = null;
       } else {
+        //If the character is not in the secret word, or all occurences of the character already matched, we add incorrect.
         result[i].result = "incorrect";
       }
     }
   }
   return result;
 }
-const testResult = wordleFeedback("Dansa", "cykla");
+const testResult = wordleFeedback("Hallå", "cykla");
 console.log(testResult);
